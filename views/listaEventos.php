7121084bd836c422600bca60f7a1581caa32b8ef
Ryl06 <ricardo_yeli07@hotmail.com>
2020-11-20 00:26:10 -0500

    Todo terminado

--- views/listaEventos.php
+++ views/listaEventos.php
@@ -1,6 +1,25 @@
 <?php
+//include("../admin/conexionDB.php");
+require("../admin/mostrarEvento.php");
 require('../views/sections/superior.php');
 ?>
+
+<!-- HAGO EL LLAMADO AL JS PARA OBTENER LOS DATOS DE CADA CAMPO DEL MODAL -->
+    <script src="../js/personalJS/obtenerDatos.js"></script>
+
+
+    <?php
+        $datosServicio = $conex->query("SELECT id_servicio,
+                                                cantidad_personas,
+                                                fecha,
+                                                TIME_FORMAT(hora_inicio,'%H:%i') AS hora_inicio,
+                                                TIME_FORMAT(hora_final,'%H:%i') AS hora_final,
+                                                ubicacion,
+                                                tipo_evento,
+                                                descripcion 
+                                                FROM servicio");   
+        $datosCliente = $conex->query( "SELECT nombre,apellido FROM cliente");
+    ?>
 
 <!-- Main Content -->
 <div class="container text-gray-900">
@@ -23,42 +42,49 @@ require('../views/sections/superior.php');
             </tr>
           </thead>
           <tbody class="text-gray-900">
+
+               <?php 
+
+                  while($datos=$datosServicio->fetch(PDO::FETCH_ASSOC)) {
+                  $clientes = $datosCliente->fetch(PDO::FETCH_ASSOC);
+
+                  
+                  $datosMostrar=$datos['id_servicio']."||".
+                                $clientes['nombre']." ".$clientes['apellido']."||".
+                                $datos['fecha']."||".   
+                                $datos['ubicacion']."||".
+                                $datos['hora_inicio']."||".
+                                $datos['hora_final']."||".
+                                $datos['tipo_evento']."||".
+                                $datos['cantidad_personas']."||".
+                                $datos['descripcion'];
+
+
+                  $datosActualizar= $datos['id_servicio']."||".
+                                    $datos['fecha']."||".   
+                                    $datos['ubicacion']."||".
+                                    $datos['hora_inicio']."||".
+                                    $datos['hora_final']."||".
+                                    $datos['descripcion'];
+                                    
+                  $datosEliminar=$datos['id_servicio'];
+                  ?>                    
+
             <tr>
-              <td role="button" data-toggle="modal" data-target="#ModalInfo"> <i class="fas fa-search fa-fw"></i> </td>
-              <td>22/11/20</td>
-              <td>Franklin Iván</td>
-              <td>Sinceramente creo que el barca, esta temporada, no ganará nada, me jode pero siendo honesto creo que será así.</td>
+                  <td role="button" data-toggle="modal" data-target="#ModalInfo" onclick="verEvento('<?php echo $datosMostrar; ?>')" class="ModalInfo"> <i class="fas fa-search fa-fw ModalInfo"></i> </td>
+                  <td><?php echo $datos['fecha']; ?></td>
+                  <td><?php echo $clientes['nombre']." ".$clientes['apellido']; ?></td>
+                  <td><?php echo $datos['descripcion']; ?></td>
               <td>
-                <button data-toggle="modal" data-target="#btnActualizar" class="btn text-white" style="background-color: #0f9bd0;">Actualizar</button>
+                <button data-toggle="modal" data-target="#btnActualizar" onclick="obtenerEvento('<?php echo $datosActualizar; ?>')" class="btn text-white" style="background-color: #0f9bd0;">Actualizar</button>
               </td>
               <td class="text-center">
-                <button data-toggle="modal" data-target="#btnEliminar" class="btn text-white" style="background-color: #b9181f;"><i class="fas fa-trash-alt fa-fw"></i></button>
+                <button data-toggle="modal" data-target="#btnEliminar" onclick="eliminaEvento('<?php echo $datosEliminar; ?>')" class="btn text-white deletebtn" style="background-color: #b9181f;" ><i class="fas fa-trash-alt fa-fw"></i></button>
               </td>
             </tr>
-            <tr>
-              <td role="button" data-toggle="modal" data-target="#ModalInfo"> <i class="fas fa-search fa-fw"></i> </td>
-              <td>24/11/20</td>
-              <td>Juan Zamora</td>
-              <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est illo maxime quis harum amet sed culpa vero voluptates.</td>
-              <td>
-                <button data-toggle="modal" data-target="#btnActualizar" class="btn text-white" style="background-color: #0f9bd0;">Actualizar</button>
-              </td>
-              <td class="text-center">
-                <button data-toggle="modal" data-target="#btnEliminar" class="btn text-white" style="background-color: #b9181f;"><i class="fas fa-trash-alt fa-fw"></i></button>
-              </td>
-            </tr>
-            <tr>
-              <td role="button" data-toggle="modal" data-target="#ModalInfo"> <i class="fas fa-search fa-fw"></i> </td>
-              <td>26/11/20</td>
-              <td>Ricardo Ye</td>
-              <td>Cha pero ta priti loco, qué locura con los botones esos loco chaa la vida.</td>
-              <td>
-                <button data-toggle="modal" data-target="#btnActualizar" class="btn text-white" style="background-color: #0f9bd0;">Actualizar</button>
-              </td>
-              <td class="text-center">
-                <button data-toggle="modal" data-target="#btnEliminar" class="btn text-white" style="background-color: #b9181f;"><i class="fas fa-trash-alt fa-fw"></i></button>
-              </td>
-            </tr>
+            <?php 
+                }         
+            ?>
           </tbody>
         </table>
       </div>
@@ -85,33 +111,39 @@ require('../views/sections/superior.php');
         </button>
       </div>
       <div class="modal-body">
-
+      <input type="hidden" id="id_servicio" name="id_servicio"></input>
         <div class="form-group">
           <label> <span class="font-weight-bold">De:</span> </label>
+          <input class="form-control font-italic" id="verNombre" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Fecha:</span> </label>
+          <input class="form-control font-italic" id="verFecha" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Ubicación:</span> </label>
+          <input class="form-control font-italic" id="verUbicacion" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Hora inicio:</span> </label>
+          <input class="form-control font-italic" id="verHoraInicial" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Hora final:</span> </label>
-        </div>
-        <div class="form-group">
-          <label> <span class="font-weight-bold">Tipo de Servicio: </span> </label>
+          <input class="form-control font-italic" id="verHoraFinal" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Tipo de Evento: </span> </label>
+          <input class="form-control font-italic" id="verTipoEvento" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Cantidad de Personas: </span> </label>
+          <input class="form-control font-italic" id="verCantidadPersonas" disabled> 
         </div>
         <div class="form-group">
           <label> <span class="font-weight-bold">Descripción: </span> </label>
+      <!--    <label class="form-control font-italic" id="verDescripcion">-->
+          <textarea  id="verDescripcion" cols="57" rows=5 disabled></textarea>
         </div>
 
       </div>
@@ -124,7 +156,7 @@ require('../views/sections/superior.php');
 <!-- End Modal info -->
 
 <!-- Modal btnActualizar -->
-
+<form  method="POST" action="../admin/actualizarEvento.php"  > 
 <div class="modal fade text-gray-900" id="btnActualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -135,26 +167,26 @@ require('../views/sections/superior.php');
         </button>
       </div>
       <div class="modal-body">
-
+          <input type="hidden" id="id_servicioA" name="id_servicioA" required>
         <div class="form-group">
           <label class="font-weight-bold">Fecha:</label>
-          <input type="date" class="form-control font-italic" name="fecha" placeholder="fecha..." required>
+          <input type="date" class="form-control font-italic" id="nuevaFecha" name="nuevaFecha" required>
         </div>
         <div class="form-group">
           <label class="font-weight-bold">Ubicación:</label>
-          <input type="text" class="form-control font-italic" name="ubicacion" placeholder="ubicación..." required>
+          <input type="text" class="form-control font-italic" id="nuevaUbicacion" name="nuevaUbicacion" required>
         </div>
         <div class="form-group">
           <label class="font-weight-bold">Hora Inicial:</label>
-          <input type="time" class="form-control font-italic" name="horaInicial" placeholder="hora inicial..." required>
+          <input type="time" class="form-control font-italic" id="nuevaHoraInicial" name="nuevaHoraInicial" required>
         </div>
         <div class="form-group">
           <label class="font-weight-bold">Hora Final:</label>
-          <input type="time" class="form-control font-italic" name="horaFinal" placeholder="hora final..." required>
+          <input type="time" class="form-control font-italic" id="nuevaHoraFinal" name="nuevaHoraFinal" required>
         </div>
         <div class="form-group">
-          <label class="font-weight-bold">Descripción:</label><br>
-          <textarea name="descripcion" id="" cols="57" rows=5></textarea>
+          <label class="font-weight-bold" >Descripción:</label><br>
+          <textarea  id="nuevaDescripcion" cols="57" rows=5 name="nuevaDescripcion"></textarea>
         </div>
         <!-- <div class="form-group">
           <label class="font-weight-bold">Color</label>
@@ -163,16 +195,17 @@ require('../views/sections/superior.php');
 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn text-white" style="background-color: #0f9bd0;">Guardar Cambios</button>
+        <button type="submit" class="btn text-white" style="background-color: #0f9bd0;">Guardar Cambios</button>
         <button type="button" class="btn btn-outline-dark" data-dismiss="modal">cancelar</button>
       </div>
     </div>
   </div>
 </div>
+</form>
 <!-- End Modal btnActualizar -->
 
 <!-- Modal btnEliminar -->
-
+<form  method="POST" action="../admin/eliminarEvento.php" > 
 <div class="modal fade text-gray-900" id="btnEliminar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -184,15 +217,32 @@ require('../views/sections/superior.php');
       </div>
       <div class="modal-body">
         ¿Estás seguro de que quieres eliminar este evento?
+        <input type="hidden" name="id_servicioE" id="id_servicioE" required>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn text-white" style="background-color: #b9181f;">Eliminar</button>
+        <button type="submit" class="btn text-white" style="background-color: #b9181f;">Eliminar</button>
         <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cancelar</button>
       </div>
     </div>
   </div>
 </div>
+</form>
 <!-- End Modal btnEliminar -->
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+
+        $('#actualizar').click(function(){
+          actualizarEvento();
+        });
+
+      $('#eliminar').click(function(){
+          eliminaEvento();
+        });
+
+      });
+
+    </script>
 
 <?php
 require('../views/sections/inferior.php');

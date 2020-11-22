94c66d0d81ade2c04ca10444b225879da5016c3d
Franklin Alvarado <franklinalvarado0921@gmail.com>
2020-11-21 21:44:29 -0600

    cambiando el calendario

--- views/bienvenido.php
+++ views/bienvenido.php
@@ -23,59 +23,47 @@ require('../views/sections/superior.php');
       var calendarEl = document.getElementById('calendar');
       var calendar = new FullCalendar.Calendar(calendarEl, {
         // Custom
+        selectable: true,
+        dayMaxEvents: true,
+        businessHours: true,
+        navLinks: true,
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,dayGridWeek,dayGridDay'
+          right: 'dayGridMonth,dayGridWeek,dayGridDay ayuda'
         },
-
-        dateClick: function(info) {
-          $('#fecha').val(info.dateStr);
-          $('#dayModal').modal();
-          console.log(info);
-          calendar.addEvent({
-            title:"Evento x",
-            date:info.dateStr
-          });
-
-          /* alert('Date: ' + info.dateStr);
-          alert('Resource ID: ' + info.resource.id); */
-        },
-
-        eventClick: function(info) {
-          //console.log(info);
-          console.log(info.event.title);
-          console.log(info.event.start);
-
-          console.log(info.event.end);
-          console.log(info.event.backgroundColor);
-          console.log(info.event.extendedProps.description);
-        },
-
-        events: [{
-            title: 'Jornada extensa',
-            start: '2020-11-07',
-            end: '2020-11-10',
-            color: '#eb164b',
-            description: 'Descripción del evento 1 mi loco'
-          },
-          {
-            title: 'Repetir evento',
-            start: '2020-11-09T16:00:00',
-            description: 'Descripción del evento 1 mi loco'
-          },
-          {
-            title: 'Repetir evento',
-            start: '2020-11-11T16:00:00',
-            description: 'Descripción del evento 2 mi loco'
-          },
-          {
-            title: 'Todo el día loco',
-            start: '2020-11-30',
-            color: '#16eb3a',
-            description: 'Descripción del evento 3 mi loco'
+        customButtons: {
+          ayuda: {
+            text: "help",
+            click: function() {
+              $('#QAyuda').modal();
+            }
           }
-        ],
+        },
+        
+        // Mostrar datos al seleccionar evento
+        eventClick: function(info) {
+          day = (info.event.start.getDate());
+          month = (info.event.start.getMonth() + 1);
+          year = (info.event.start.getFullYear());
+
+          $('#tituloEvento').html(info.event.title);
+          $('#fechaEvento').html(month + "/" + day + "/" + year);
+          $('#ubicacionEvento').html(info.event.extendedProps.ubicacion);
+          $('#horaIniEvento').html(info.event.extendedProps.hora_inicio);
+          $('#horaFinEvento').html(info.event.extendedProps.hora_final);
+          $('#tipoServEvento').html(info.event.extendedProps.tipo_servicio);
+          $('#tipoEvenEvento').html(info.event.extendedProps.tipo_evento);
+          $('#cantidadPerEvento').html(info.event.extendedProps.cantidad_personas);
+          $('#tituloEventoDes').html(info.event.title);
+          $('#descripcionEvento').html(info.event.extendedProps.descripcion);
+          $('#eventsModal').modal();
+        },
+
+        // Listar eventos desde la BD
+        events: '../admin/calendar/eventos.php',
+        //Color para en espera: lightslategray
+        //Color para aceptados: mediumseagreen
 
         initialView: 'dayGridMonth'
         // End of Custom
@@ -83,56 +71,80 @@ require('../views/sections/superior.php');
       calendar.setOption('locale', 'es');
       calendar.render();
 
-      function recolectarDatosGUI(method){
-        nuevoEvento={
-          
-        }
-      }
-
     });
   </script>
 
   <!-- Calendar -->
   <div id='calendar' style="font-family:Arial, Helvetica, sans-serif"></div>
 
-  <!-- Day Modal -->
-  <div class="modal fade" id="dayModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <!-- Events Modal -->
+  <div class="modal fade" id="eventsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Evento</h5>
+        <div class="modal-header" style="background-color: #68086c;">
+          <h5 class="modal-title text-white" id="tituloEvento"></h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+            <span class="text-white" aria-hidden="true">&times;</span>
           </button>
         </div>
+
         <div class="modal-body">
 
           <div class="form-group">
-            <label> <span class="font-weight-bold">De:</span> Fial</label>
+            <label> <span class="font-weight-bold">De:</span> Franklin Iván</label>
+            <input type="hidden" name="nombre" id="nombre" value="Fraklooon loco">
           </div>
           <div class="form-group">
-            <label> <span class="font-weight-bold">Fecha:</span>  <input id="fecha" disabled aria-label="Default" aria-describedby="inputGroup-sizing-default"> </label>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="font-weight-bold">Fecha del Evento:</label>
+                <label class="form-control font-italic" id="fechaEvento">
+              </div>
+              <div class="col-md-6">
+                <label class="font-weight-bold">Estado:</label>
+                <label class="form-control font-italic" readonly>Pendiente</label>
+              </div>
+            </div>
           </div>
           <div class="form-group">
-            <label> <span class="font-weight-bold">Ubicación:</span> cha en mi casa loco</label>
+            <label class="font-weight-bold">Ubicación:</label>
+            <label class="form-control font-italic" id="ubicacionEvento">
           </div>
           <div class="form-group">
-            <label> <span class="font-weight-bold">Hora inicio:</span> 9:00 am</label>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="font-weight-bold">Hora Inicio:</label>
+                <label class="form-control font-italic" id="horaIniEvento">
+              </div>
+              <div class="col-md-6">
+                <label class="font-weight-bold">Hora Final:</label>
+                <label class="form-control font-italic" id="horaFinEvento">
+              </div>
+            </div>
           </div>
           <div class="form-group">
-            <label> <span class="font-weight-bold">Hora final:</span> 3:00 pm</label>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="font-weight-bold">Tipo de Servicio:</label>
+                <label class="form-control font-italic" id="tipoServEvento">
+              </div>
+              <div class="col-md-6">
+                <label class="font-weight-bold">Tipo de Evento:</label>
+                <label class="form-control font-italic" id="tipoEvenEvento">
+              </div>
+            </div>
           </div>
           <div class="form-group">
-            <label>  <span class="font-weight-bold">Tipo de Servicio: </span> Privado</label>
+            <label> <span class="font-weight-bold">Cantidad de Personas:</span></label>
+            <label class="form-control font-italic" id="cantidadPerEvento">
           </div>
           <div class="form-group">
-          <label>  <span class="font-weight-bold">Tipo de Evento: </span> Graduación</label>
+            <label class="font-weight-bold">Título Evento:</label><br>
+            <label class="form-control font-italic" id="tituloEventoDes">
           </div>
           <div class="form-group">
-          <label>  <span class="font-weight-bold">Cantidad de Personas: </span> 14</label>
-          </div>
-          <div class="form-group">
-          <label>  <span class="font-weight-bold">Descripción: </span> ete setch</label>
+            <label class="font-weight-bold">Descripción:</label><br>
+            <textarea class="form-control font-italic" readonly id="descripcionEvento" cols="57" rows=5></textarea>
           </div>
 
         </div>
@@ -140,10 +152,10 @@ require('../views/sections/superior.php');
         <div class="modal-footer">
           <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cerrar</button>
         </div>
+
       </div>
     </div>
   </div>
-  <!-- End Day Modal -->
 
   <!-- End Calendar -->
 
@@ -151,11 +163,9 @@ require('../views/sections/superior.php');
 
 <!-- End of Main Content -->
 <script>
-
-document.getElementById("agenda").style.backgroundColor = "#920896";
-document.getElementById("agendaTitulo").style.color = "white";
-document.getElementById("agendaIcon").style.color = "white";
-
+  document.getElementById("agenda").style.backgroundColor = "#920896";
+  document.getElementById("agendaTitulo").style.color = "white";
+  document.getElementById("agendaIcon").style.color = "white";
 </script>
 
 <?php
